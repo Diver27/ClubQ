f1d1f58064abb9da2b8a8bc30badd204c164cadc
Diver-Animus <yinliuxuan@gmail.com>
2020-05-11 23:29:12 +0800

    注释

--- src/main/java/diver27/clubq/controller/EventController.java
+++ src/main/java/diver27/clubq/controller/EventController.java
@@ -18,30 +18,89 @@ public class EventController {
     @Autowired
     private EventService eventService;
 
+    /**
+     * 最近发布社团活动列表
+     * @param head 列表起始位置
+     * @param pageLimit 列表长度
+     * @return
+     * [
+     *     {
+     *         "id": 4,
+     *         "title": "test4",
+     *         "clubId": 3,
+     *         "time": "2020-05-22T14:03:02.000+0000",
+     *         "location": "testplace4",
+     *         "description": "testintro4",
+     *         "status": 3
+     *     }
+     * ]
+     */
     @GetMapping(path = "/latest")
     public @ResponseBody
     Iterable<Event> getEventLatest(@RequestParam int head, @RequestParam int pageLimit) {
         return eventService.getEventsLatest(head, pageLimit);
     }
 
+    /**
+     * 随机取得活动列表
+     * @param pageLimit 列表长度
+     * @return 同上
+     */
     @GetMapping(path = "/random")
     public @ResponseBody
     Iterable<Event> getEventRandom(@RequestParam int pageLimit) {
         return eventService.getEventsRandom(pageLimit);
     }
 
+    /**
+     * 获得活动详情
+     * @param eventId 活动id
+     * @return
+     * {
+     *     "id": 1,
+     *     "title": "test1",
+     *     "clubId": 1,
+     *     "time": "2019-11-20T00:00:00.000+0000",
+     *     "location": "testplace1",
+     *     "description": "testintro1",
+     *     "status": 1
+     * }
+     */
     @GetMapping(path = "/info")
     public @ResponseBody
     Optional<Event> getEventInfo(@RequestParam Integer eventId) {
         return eventService.getEventInfo(eventId);
     }
 
+    /**
+     * 获取已参与的活动列表
+     * @param userId 用户id
+     * @return
+     * [
+     *     {
+     *         "id": 1,
+     *         "title": "test1",
+     *         "clubId": 1,
+     *         "time": "2019-11-20T00:00:00.000+0000",
+     *         "location": "testplace1",
+     *         "description": "testintro1",
+     *         "status": 1
+     *     }
+     * ]
+     */
     @GetMapping(path = "/my_event")
     public @ResponseBody
     List<Event> getMyEvent(@RequestParam Integer userId){
         return eventService.getMyEvent(userId);
     }
 
+    /**
+     * 依名称搜索活动
+     * @param keyword 名称关键词
+     * @param head 列表起始
+     * @param pageLimit 列表长度
+     * @return 同上
+     */
     @GetMapping(path="/search")
     public @ResponseBody
     List<Event> searchEvents(@RequestParam String keyword, @RequestParam int head, @RequestParam int pageLimit){
--- src/main/java/diver27/clubq/controller/UserController.java
+++ src/main/java/diver27/clubq/controller/UserController.java
@@ -14,18 +14,42 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    /**
+     * 新增用户
+     * @param username 用户名
+     * @return
+     * 布尔值：  true
+     */
     @PostMapping(path = "/new")
     public @ResponseBody
     boolean newUser(@RequestParam String username) {
         return userService.newUser(username);
     }
 
+    /**
+     * 查看用户信息
+     * @param userId 用户id
+     * @return
+     * {
+     *     "id": 1,
+     *     "username": "diver27",
+     *     "infoText": "test1",
+     *     "profileImageId": null
+     * }
+     */
     @GetMapping(path = "/info")
     public @ResponseBody
     Optional<User> getUserInfo(@RequestParam Integer userId) {
         return userService.getUserInfo(userId);
     }
 
+    /**
+     * 更新用户简介
+     * @param userId 用户id
+     * @param infoText 简介文字
+     * @return
+     * 布尔值: true
+     */
     @PostMapping(path = "/update")
     public @ResponseBody
     boolean updateUserInfo(@RequestParam Integer userId, @RequestParam String infoText) {
